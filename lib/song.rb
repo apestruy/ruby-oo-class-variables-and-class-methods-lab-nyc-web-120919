3c30226cd4a0e6b6f9f53f0298034e0e919510f5
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-11 23:56:17 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres << self.genre   
+    @@artists << self.artist 
+  end 
+  
+  def self.genres 
+    @@genres
+  end 
+  
+  def self.artists 
+    @@artists.find do |artist|
+  end 
+  
+  def self.genre_count 
+  
+  end 
+end 
