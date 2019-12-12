cbb2695236ad597564a5b14fb257aee1518ace83
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-12 00:07:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,14 @@ class Song
   
   def self.artist_count 
     hash = {}
-    
+    @@artists.each do |artist|
+      if hash[artist]
+        hash[artist] += 1 
+      else 
+        hash[artist] = 1 
+      end 
+    end 
+    hash 
   end 
 end 
 
